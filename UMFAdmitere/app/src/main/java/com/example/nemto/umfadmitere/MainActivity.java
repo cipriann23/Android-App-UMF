2bf44e221e84f7abcea40e9aeb4e8b1e81c3638a
cipriann23 <nemtocciprian10@gmail.com>
2017-08-03 06:31:27 +0300

    Year selection

--- UMFAdmitere/app/src/main/java/com/example/nemto/umfadmitere/MainActivity.java
+++ UMFAdmitere/app/src/main/java/com/example/nemto/umfadmitere/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,18 +18,50 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Biologie button */
     public void buttonBiologie (View view) {
+
+        boolean isChecked = ((CheckBox) findViewById(R.id.currentYear)).isChecked();
+        boolean isCheckedAll = ((CheckBox) findViewById(R.id.lastYears)).isChecked();
+        String year;
+
+        if(isChecked==true && isCheckedAll==false){
+            year = "last";
+        }else{
+            year = "all";
+        }
+
+        if(isCheckedAll){
+            year = "all";
+        }
+
         Intent intent = new Intent(this, RandomQuestion.class);
         String category = "biologie";
-        intent.putExtra("name_key",category);
+        intent.putExtra("category",category);
+        intent.putExtra("year",year);
         startActivity(intent);
     }
 
 
     /** Called when the user taps the Chimie button */
     public void buttonChimie (View view) {
+
+        boolean isChecked = ((CheckBox) findViewById(R.id.currentYear)).isChecked();
+        boolean isCheckedAll = ((CheckBox) findViewById(R.id.lastYears)).isChecked();
+        String year;
+
+        if(isChecked==true && isCheckedAll==false){
+            year = "last";
+        }else{
+            year = "all";
+        }
+
+        if(isCheckedAll){
+            year = "all";
+        }
+
         Intent intent = new Intent(this, RandomQuestion.class);
         String category = "chimie";
-        intent.putExtra("name_key",category);
+        intent.putExtra("category",category);
+        intent.putExtra("year",year);
         startActivity(intent);
     }
 }
--- UMFAdmitere/app/src/main/java/com/example/nemto/umfadmitere/RandomQuestion.java
+++ UMFAdmitere/app/src/main/java/com/example/nemto/umfadmitere/RandomQuestion.java
@@ -19,14 +19,14 @@ public class RandomQuestion extends AppCompatActivity {
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String category = intent.getStringExtra("name_key");
-
+        String category = intent.getStringExtra("category");
+        String year = intent.getStringExtra("year");
         // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(category);
 
         BackgroundWorker backgroundWorker = new BackgroundWorker(this);
-        backgroundWorker.execute("2017",category);
+        backgroundWorker.execute(year,category);
 
         ///////--------------------------------
 

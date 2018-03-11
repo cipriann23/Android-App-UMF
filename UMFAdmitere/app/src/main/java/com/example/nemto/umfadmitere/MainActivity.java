99e8825e14450937cc7d6a24f3cb94bcad9b94a5
cipriann23 <nemtocciprian10@gmail.com>
2018-03-11 14:06:00 +0200

    Responsive

--- UMFAdmitere/app/src/main/java/com/example/nemto/umfadmitere/MainActivity.java
+++ UMFAdmitere/app/src/main/java/com/example/nemto/umfadmitere/MainActivity.java
@@ -4,13 +4,7 @@ package com.example.nemto.umfadmitere;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Handler;
-import android.preference.PreferenceManager;
-import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,11 +13,8 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.ToggleButton;
 
 import java.util.Objects;
 
